77da144047a7db06a333686d173b2fa896bd9eb5
CamTabUnivalle <juan.camilo.taborda@correounivalle.edu.co>
2025-10-26 15:36:50 -0500

    Add login component

--- client/src/router/App.tsx
+++ client/src/router/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
     element: <Layout />,
     children: [
       {
