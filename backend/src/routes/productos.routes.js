3abde0c443ace0afaa5d618a14ded07920fa1e7e
Juan David Garzon <juanxavif@gmail.com>
2025-11-19 06:32:03 -0500

    modificando las rutas de los productos

--- backend/src/routes/productos.routes.js
+++ backend/src/routes/productos.routes.js
@@ -1,15 +1,20 @@
+// routes/productos.routes.js
 import express from 'express';
 import {
   getProductos,
   getProductoById,
   createProducto,
   updateProducto,
-  deleteProducto
+  deleteProducto,
+  getProductosPorCategoria,
+  buscarProductos
 } from '../controllers/productos.controller.js';
 
 const router = express.Router();
 
 router.get('/', getProductos);
+router.get('/buscar', buscarProductos);
+router.get('/categoria/:id_categoria', getProductosPorCategoria);
 router.get('/:id', getProductoById);
 router.post('/', createProducto);
 router.put('/:id', updateProducto);
